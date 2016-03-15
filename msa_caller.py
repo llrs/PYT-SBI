ddf82746b5f1fda9d19492f5bd5301d27225817a
Lluís Revilla <lluis.revilla@gmail.com>
2016-03-15 14:50:51 +0100

    Make a requirement of the input file and set some other minor changes

--- msa_caller.py
+++ msa_caller.py
@@ -7,6 +7,7 @@ Created on Mar 13, 2016
 # standard modules
 import os
 import argparse
+import logging
 from distutils.spawn import find_executable
 
 # Biopython modules
@@ -14,32 +15,37 @@ from Bio.Align.Applications import ClustalwCommandline
 from Bio.Align.Applications import MuscleCommandline
 from Bio.Align.Applications import TCoffeeCommandline
 
-def call_msa_method(method_label, in_file, out_file, executable_path):
-	"""Calls the appropriate MSA method"""
-	if method_label == "clustalw":
-		cline = ClustalwCommandline(executable_path, 
-								    infile = in_file)
-	elif method_label == "muscle":
-		cline = MuscleCommandline(executable_path, 
-								  input = in_file, 
-								  out = out_file)
-	elif method_label == "t_coffee":
-		cline = TCoffeeCommandline(infile = in_file,
-								   output = "clustalw",
-								   outfile = out_file)
-	else:
-		return
-	assert os.path.isfile(executable_path), "Executable missing"
-	stdout, stderr = cline()
+
+def call_msa_method(method, in_file, out_file, executable_path, output_format):
+    """Calls the appropriate MSA method"""
+    logging.info("Creating a MSA  with {} from {} to {}".format(
+                                            method, in_file, out_file))
+    assert os.path.isfile(executable_path), "Executable missing"
+    if method == "clustalw":
+        cline = ClustalwCommandline(executable_path,
+                                    infile=in_file, output_format="fasta")
+    elif method == "muscle":
+        cline = MuscleCommandline(executable_path,
+                                  input=in_file,
+                                  out=out_file)
+    elif method == "t_coffee":
+        cline = TCoffeeCommandline(infile=in_file,
+                                   output="clustalw",
+                                   outfile=out_file)
+
+    stdout, stderr = cline()
+    logging.debug(stderr)
+    return(stdout)
 
 if __name__ == '__main__':
-	msg = 'Carries out a MSA'
-	argparser = argparse.ArgumentParser(description=msg)
-	argparser.add_argument("-f",
-						   help="File with sequences to be aligned.")
-	argparser.add_argument("-m", 
+    msg = 'Carries out a MSA'
+    programs_MSA = ["clustalw", "muscle", "t_coffee"]
+    argparser = argparse.ArgumentParser(description=msg)
+    argparser.add_argument("file",
+                           help="File with sequences to be aligned.")
+    argparser.add_argument("-m",
                            help="Method for MSA.",
-                           default="clustalw")
-	argparser.add_argument("-o", help="Output file.")
-	args = argparser.parse_args()
-	call_msa_method(args.m, args.f, "aligned.aln", find_executable(args.m))
+                           default="clustalw", choices=programs_MSA)
+    argparser.add_argument("-o", help="Output file.")
+    args = argparser.parse_args()
+    call_msa_method(args.m, args.f, "aligned.aln", find_executable(args.m))
